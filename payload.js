0d43faeb3761944c38ef7971b44e0e46a1abdd66
staram <10952725+staram@users.noreply.github.com>
2024-02-14 20:24:51 +0000

    Add : to list of illegal characters in savefile name

--- payload.js
+++ payload.js
@@ -1,7 +1,10 @@
 // Retrieve the title and URL of the current webpage
-var title = document.title.replace(/[|&;$%@"<>()+,]/g, "-");
+var title = document.title.replace(/[|&:;$%@"<>()+,]/g, "-");
 var docURL = window.location.href;
 
 
 // send the page title as a chrome message
-chrome.runtime.sendMessage({ messageTitle: title, messageURL: docURL });
+chrome.runtime.sendMessage({ 
+    messageTitle: title, 
+    messageURL: docURL 
+});
