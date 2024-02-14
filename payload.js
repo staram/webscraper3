ceea25405554e139cc31090b7b9ed343c823b85a
staram <10952725+staram@users.noreply.github.com>
2024-02-14 14:04:17 +0000

    Initial commit

---
+++ payload.js
@@ -0,0 +1,7 @@
+// Retrieve the title and URL of the current webpage
+var title = document.title;
+var docURL = window.location.href;
+
+
+// send the page title as a chrome message
+chrome.runtime.sendMessage({ messageTitle: title, messageURL: docURL });
---
+++ popup.js
@@ -0,0 +1,37 @@
+// Inject the payload.js script into the current tab after the popout has loaded
+
+window.addEventListener("load", function(e) {
+	chrome.extension.getBackgroundPage().chrome.tabs.executeScript(null, {
+		file: "payload.js"
+	});;
+});
+
+// Listen to messages from the payload.js script and write to popout.html
+chrome.runtime.onMessage.addListener(function (message) {
+    document.getElementById('page-url').value = message.messageURL;
+	document.getElementById('page-title').value = message.messageTitle;
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('savebutton').addEventListener("click", saveTextToFile);
+});
+
+function saveTextToFile() {
+    const textToSave = document.getElementById("page-text").value;
+
+    if (!textToSave) {
+        alert("Please enter some text to save.");
+        return;
+    }
+
+    const blob = new Blob([textToSave], {type: 'text/plain;charset=utf8'});
+    const url = URL.createObjectURL(blob);
+
+    chrome.downloads.download({
+        url: url,
+        filename: "saved_text.txt",
+        saveAs: true
+    });
+
+    URL.revokeObjectURL(url);
+}
